aea171b245ba9e10a3e96c4c312a65c5dec69b62
0xPorkchops <proulxcameron@icloud.com>
2025-03-21 18:15:36 -0400

    Added template chart to dashboard

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { createColumns } from "@/components/table/columns"
 import { DataTable } from "@/components/table/data-table"
 import { useUserContext } from "@/components/user-context";
+import { DataChart } from "@/components/data-chart";
 
 export default function DemoPage() {
   const { getAllEntries, deleteEntry } = useUserContext();
@@ -9,6 +10,7 @@ export default function DemoPage() {
 
   return (
     <div className="container mx-auto py-10">
+      <DataChart />
       <DataTable columns={columns} data={carbonEntries} />
     </div>
   )
