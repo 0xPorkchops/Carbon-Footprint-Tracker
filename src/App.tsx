50f25c6beff0c8d1dd69620044527e366bf018ff
0xPorkchops <proulxcameron@icloud.com>
2025-03-21 23:55:40 -0400

    Readded sidebar collapse button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { UserProvider } from "@/components/user-context";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes";
@@ -11,6 +11,11 @@ export default function App() {
         <Router>
           <AppSidebar variant="floating" />
           <div className="flex flex-col flex-1">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+              </div>
+            </header>
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
               <AppRoutes />
               <div className="grid auto-rows-min gap-4 md:grid-cols-3">
